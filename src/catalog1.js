1be3e5d1c461485aa96b31fd041a95006b86ed8b
srtxcheatr <lamatemba087@gmail.com>
2026-08-06 08:43:17 +0545

    Update catalog1.js

--- src/catalog1.js
+++ src/catalog1.js
@@ -4,37 +4,16 @@
 
 export const CATALOG = {
 
-// ---- BALA XYZ CHEATS FF NONROOT (pid 133) ----
-sku_126: {
-  pid: '133',
-  row: 'BALA XYZ CHEATS FF NONROOT',
-  name: 'BALA XYZ 1 HOUR',
-  duration: '1 Hour',
-  price: 100,           // set your actual price
-  image: 'https://i.postimg.cc/...', // add your image URL
-  external: true,
-  requiresAndroidId: true,
-},
-sku_127: {
-  pid: '133',
-  row: 'BALA XYZ CHEATS FF NONROOT',
-  name: 'BALA XYZ 3 HOURS',
-  duration: '3 Hours',
-  price: 250,
-  image: 'https://i.postimg.cc/...',
-  external: true,
-  requiresAndroidId: true,
-},
-sku_128: {
-  pid: '133',
-  row: 'BALA XYZ CHEATS FF NONROOT',
-  name: 'BALA XYZ 6 HOURS',
-  duration: '6 Hours',
-  price: 450,
-  image: 'https://i.postimg.cc/...',
-  external: true,
-  requiresAndroidId: true,
-},
+sku_126: { pid: '133', row: 'BALA XYZ CHEATS FF NONROOT', name: 'BALA XYZ 1 HOUR', duration: '1 Hour', price: 100, image: 'https://i.postimg.cc/v80BBj0b/file-000000001864820bbc9fa4acd79301df.png', external: true },
+sku_127: { pid: '133', row: 'BALA XYZ CHEATS FF NONROOT', name: 'BALA XYZ 3 HOUR', duration: '3 Hour', price: 350, image: 'https://i.postimg.cc/v80BBj0b/file-000000001864820bbc9fa4acd79301df.png', external: true },
+sku_128: { pid: '450', row: 'BALA XYZ CHEATS FF NONROOT', name: 'BALA XYZ 6 HOUR', duration: '6 Hour', price: 300, image: 'https://i.postimg.cc/v80BBj0b/file-000000001864820bbc9fa4acd79301df.png', external: true },
+
+
+
+// ---- HAXX-CKER PRO FF ROOT ANDROID (pid 64) ----
+  sku_129: { pid: '64', row: 'HAXX-CKER PRO FF NONROOT ANDROID', name: 'HAXX-CKER 10 DAY', duration: '10 DaYs', price: 714, image: 'https://i.postimg.cc/HLxmsSCy/Screenshot-20260425-150832.jpg', external: true },
+  sku_130: { pid: '64', row: 'HAXX-CKER PRO FF NONROOT ANDROID', name: 'HAXX-CKER 20 DAY', duration: '20 DaYs', price: 1360, image: 'https://i.postimg.cc/HLxmsSCy/Screenshot-20260425-150832.jpg', external: true },
+
   // ---- APK MC PANEL FF ROOT ANDROID (pid 124) ----
   sku_1: { pid: '124', row: 'APK MC PANEL FF ROOT ANDROID', name: 'APK MC PANEL 1 DAY', duration: '1 DaYs', price: 111, image: 'https://i.postimg.cc/Znxb1WYr/Screenshot-20260723-144925.jpg', external: true },
   sku_2: { pid: '124', row: 'APK MC PANEL FF ROOT ANDROID', name: 'APK MC PANEL 3 DAY', duration: '3 DaYs', price: 204, image: 'https://i.postimg.cc/Znxb1WYr/Screenshot-20260723-144925.jpg', external: true },
