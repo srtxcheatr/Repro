5d55acf24dd5390c8308995682ebd0eeea3dae71
srtxcheatr <lamatemba087@gmail.com>
2026-08-15 11:20:00 +0545

    Update catalog1.js

--- src/catalog1.js
+++ src/catalog1.js
@@ -15,7 +15,7 @@ export const CATALOG = {
   sku_146: { pid: '131', row: 'BALA MOD XYZ ~ V2 FF NONROOT', name: 'BALA MOD XYZ 3 DAY', duration: '3 DaYs', price: 1700, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true },
   sku_148: { pid: '131', row: 'BALA MOD XYZ ~ V2 FF NONROOT', name: 'BALA MOD XYZ 5 DAY', duration: '5 DaYs', price: 2600, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true },
   sku_149: { pid: '131', row: 'BALA MOD XYZ ~ V2 FF NONROOT', name: 'BALA MOD XYZ 7 DAY', duration: '7 DaYs', price: 3250, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true },
-},
+
 
 // ---- HAXX-CKER PRO FF ROOT ANDROID (pid 64) ----
   sku_129: { pid: '64', row: 'HAXX-CKER PRO FF NONROOT ANDROID', name: 'HAXX-CKER 10 DAY', duration: '10 DaYs', price: 821, image: 'https://i.postimg.cc/HLxmsSCy/Screenshot-20260425-150832.jpg', external: true },
