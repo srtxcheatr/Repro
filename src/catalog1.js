8c29dc6575c8f410ddeb69f314697a0d9e4dee32
srtxcheatr <lamatemba087@gmail.com>
2026-08-21 17:29:12 +0545

    Update catalog1.js

--- src/catalog1.js
+++ src/catalog1.js
@@ -7,10 +7,10 @@ export const CATALOG = {
 
 
 
-sku_154: { pid: '142', row: 'BALA MOD XYZ CONFIG FF NONROOT', name: 'BALA MOD XYZ 1H', duration: '1 Hours', price: 95, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/html-body-margin0-important-padding0.html' },
-sku_155: { pid: '142', row: 'BALA MOD XYZ CONFIG FF NONROOT', name: 'BALA MOD XYZ 3H', duration: '3 Hours', price: 220, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/html-body-margin0-important-padding0.html' },
-sku_156: { pid: '142', row: 'BALA MOD XYZ CONFIG FF NONROOT', name: 'BALA MOD XYZ 6H', duration: '6 Hours', price: 390, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/html-body-margin0-important-padding0.html' },
-sku_157: { pid: '142', row: 'BALA MOD XYZ CONFIG FF NONROOT', name: 'BALA MOD XYZ 12H', duration: '12 Hours', price: 620, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/html-body-margin0-important-padding0.html' },
+sku_154: { pid: '142', row: 'BALA MOD XYZ CONFIG FF NONROOT', name: 'BALA MOD XYZ 1H', duration: '1 Hours', price: 95, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/html-body-margin0-important-padding0_02084626113.html' },
+sku_155: { pid: '142', row: 'BALA MOD XYZ CONFIG FF NONROOT', name: 'BALA MOD XYZ 3H', duration: '3 Hours', price: 220, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/html-body-margin0-important-padding0_02084626113.html' },
+sku_156: { pid: '142', row: 'BALA MOD XYZ CONFIG FF NONROOT', name: 'BALA MOD XYZ 6H', duration: '6 Hours', price: 390, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/html-body-margin0-important-padding0_02084626113.html' },
+sku_157: { pid: '142', row: 'BALA MOD XYZ CONFIG FF NONROOT', name: 'BALA MOD XYZ 12H', duration: '12 Hours', price: 620, image: 'https://i.postimg.cc/Y0HwXZkt/IMG-20260430-190421-873.jpg', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/html-body-margin0-important-padding0_02084626113.html' },
 
 // ---- HG CHEATS PRIME PROXY FF NONROOT (pid 141) ----
 // Sorted: 1, 7, 10 Days
