9f0fdc17666c058387531acf62941b05009f4b8c
srtxcheatr <lamatemba087@gmail.com>
2026-08-29 08:30:04 +0545

    Update catalog1.js

--- src/catalog1.js
+++ src/catalog1.js
@@ -6,6 +6,12 @@ export const CATALOG = {
   // ... (all existing SKUs up to sku_125) ...
 
 
+
+// ---- DELTA PROXY IOS IPHONE (pid 140) ----
+sku_170: { pid: '140', row: 'DELTA PROXY IOS IPHONE', name: 'DELTA PROXY 1 DAY', duration: '1 Day', price: 450, image: 'https://i.postimg.cc/766swCqh/file-000000002a9c82089f5e090601742428.png', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/ios-root-bg-a0b0f1a-bg-b151022.html' },
+sku_171: { pid: '140', row: 'DELTA PROXY IOS IPHONE', name: 'DELTA PROXY 7 DAY', duration: '7 Days', price: 850, image: 'https://i.postimg.cc/766swCqh/file-000000002a9c82089f5e090601742428.png', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/ios-root-bg-a0b0f1a-bg-b151022.html' },
+sku_172: { pid: '140', row: 'DELTA PROXY IOS IPHONE', name: 'DELTA PROXY 30 DAY', duration: '30 Days', price: 1550, image: 'https://i.postimg.cc/766swCqh/file-000000002a9c82089f5e090601742428.png', external: true, apkUrl: 'https://srtkeypass.blogspot.com/2026/08/ios-root-bg-a0b0f1a-bg-b151022.html' },
+
 // ---- AIM HACK FF NONROOT (pid 133) ----
 sku_160: { pid: '133', row: 'AIM HACK FF NONROOT', name: 'AIM HACK 1H', duration: '1 Hours', price: 80, image: 'https://i.postimg.cc/your-image-url.jpg', external: true, apkUrl: 'https://www.mediafire.com/file/25g8nhx1ir9yupu/AIM+HACK+NONROOT+MAIN+ID+SAFE.zip/file' },
 sku_161: { pid: '133', row: 'AIM HACK FF NONROOT', name: 'AIM HACK 3H', duration: '3 Hours', price: 160, image: 'https://i.postimg.cc/your-image-url.jpg', external: true, apkUrl: 'https://www.mediafire.com/file/25g8nhx1ir9yupu/AIM+HACK+NONROOT+MAIN+ID+SAFE.zip/file' },
