4d5d2a7c37276bb8de3bf4e2785c127a789c197a
srtxcheatr <lamatemba087@gmail.com>
2026-08-29 21:13:38 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -84,7 +84,7 @@ export function requireAdmin(req, res, next) {
 const ALLOWED_ORIGIN_SUFFIXES = ['.srt.hdhchbididjd'];
 const ALLOWED_EXACT_ORIGINS = [
   'https://bronzx.web.app',
-  'https://bronzx.firebaseapp.com','https://srtstorev5.onrender.com','https://srtstore.ct.ws','https://srtstore.ct.ws',
+  'https://bronzx.firebaseapp.com','https://srtstorev5.onrender.com','https://srtstore.ct.ws','https://srtxcheats.site.je',
 ];
 
 export const userCors = cors({
